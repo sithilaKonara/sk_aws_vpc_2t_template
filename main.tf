a10914430c6107e4b22c682b54b3ff5b48436180
mppuser-at-793820306412 <Sithila.Madushanka@pearson.com>
2023-07-16 18:32:46 +0530

    test variables

--- main.tf
+++ main.tf
@@ -17,29 +17,29 @@ locals {
 }
 
 output "json_data" {
-  value = local.json_data
+  value = length(local.json_data.vpc)
 }
 
 
 
 module "vpc" {
-
-  source = "terraform-aws-modules/vpc/aws"
   count  = length(local.json_data.vpc)
+  source = "terraform-aws-modules/vpc/aws"
 
-  name = local.json_data.vpc[count.index-1].name
-  cidr = local.json_data.vpc[count.index-1].cidr
 
-  azs             = local.json_data.vpc[count.index-1].azs
-  private_subnets = local.json_data.vpc[count.index-1].private_subnets
-  public_subnets  = local.json_data.vpc[count.index-1].public_subnets
+  name = local.json_data.vpc[count.index].name
+  cidr = local.json_data.vpc[count.index].cidr
 
-  enable_nat_gateway = local.json_data.vpc[count.index-1].enable_nat_gateway
-  enable_vpn_gateway = local.json_data.vpc[count.index-1].enable_vpn_gateway
+  azs             = local.json_data.vpc[count.index].azs
+  private_subnets = local.json_data.vpc[count.index].private_subnets
+  public_subnets  = local.json_data.vpc[count.index].public_subnets
+
+  enable_nat_gateway = local.json_data.vpc[count.index].enable_nat_gateway
+  enable_vpn_gateway = local.json_data.vpc[count.index].enable_vpn_gateway
 
   tags = {
-    Terraform   = "${local.json_data.vpc[count.index-1].tags.Terraform}"
-    Environment = "${local.json_data.vpc[count.index-1].tags.Terraform}"
+    Terraform   = "${local.json_data.vpc[count.index].tags.Terraform}"
+    Environment = "${local.json_data.vpc[count.index].tags.Terraform}"
   }
 }
 
