d4af96418b78959d83bd4e15293e1555527e794b
mppuser-at-793820306412 <Sithila.Madushanka@pearson.com>
2023-07-16 15:21:30 +0530

    push main.tf and provider.tf

--- provider.tf
+++ provider.tf
@@ -17,15 +17,3 @@ provider "aws" {
   # Configuration options
   region = "us-east-1"
 }
-
-#     terraform {
-#       backend "remote" {
-#         # The name of your Terraform Cloud organization.
-#         organization = "example-organization"
-#
-#         # The name of the Terraform Cloud workspace to store Terraform state files in.
-#         workspaces {
-#           name = "example-workspace"
-#         }
-#       }
-#     }
