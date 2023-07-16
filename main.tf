4a60b94155d0b1357f7b648eda95d6a1cad27030
mppuser-at-793820306412 <Sithila.Madushanka@pearson.com>
2023-07-16 16:59:24 +0530

    vpc test

--- main.tf
+++ main.tf
@@ -1,8 +1,33 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "sk-my-bucket"
+resource "random_string" "random" {
+  length  = 4
+  special = false
+  upper   = false
+}
+
+# Creating a resource reference point to be used by Terraform components 
+data "aws_caller_identity" "current" {}
+data "aws_partition" "current" {}
+data "aws_region" "current" {}
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
 
   tags = {
-    Name        = "sk-my-bucket"
-    Environment = "Dev"
+    Terraform   = "true"
+    Environment = "dev"
   }
 }
