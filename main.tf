1616c7b90e1d17f54b8391dc95dc58fca1b9fb39
mppuser-at-793820306412 <Sithila.Madushanka@pearson.com>
2023-07-17 09:24:11 +0530

    test object variable

--- main.tf
+++ main.tf
@@ -33,19 +33,19 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
 
-  name = local.json_data.vpc[count.index].name
-  cidr = local.json_data.vpc[count.index].cidr
+  name = var.v_vpc[count.index].name
+  cidr = var.v_vpc[count.index].cidr
 
-  azs             = local.json_data.vpc[count.index].azs
-  private_subnets = local.json_data.vpc[count.index].private_subnets
-  public_subnets  = local.json_data.vpc[count.index].public_subnets
+  azs             = var.v_vpc[count.index].azs
+  private_subnets = var.v_vpc[count.index].private_subnets
+  public_subnets  = var.v_vpc[count.index].public_subnets
 
-  enable_nat_gateway = local.json_data.vpc[count.index].enable_nat_gateway
-  enable_vpn_gateway = local.json_data.vpc[count.index].enable_vpn_gateway
+  enable_nat_gateway = var.v_vpc[count.index].enable_nat_gateway
+  enable_vpn_gateway = var.v_vpc[count.index].enable_vpn_gateway
 
   tags = {
-    Terraform   = "${local.json_data.vpc[count.index].tags.Terraform}"
-    Environment = "${local.json_data.vpc[count.index].tags.Terraform}"
+    Terraform   = "${var.v_vpc[count.index].tags["Terraform"]}"
+    Environment = "${var.v_vpc[count.index].tags["Environment"]}"
   }
 }
 
