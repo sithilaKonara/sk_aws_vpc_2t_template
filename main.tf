904f02dc35baa160da2381c660053525a7363d85
mppuser-at-793820306412 <Sithila.Madushanka@pearson.com>
2023-07-17 09:11:50 +0530

    test object variable

--- main.tf
+++ main.tf
@@ -1,29 +1,35 @@
-resource "random_string" "random" {
-  length  = 4
-  special = false
-  upper   = false
-}
+# locals {
+#   json_data = jsondecode(file("variables.json"))
+# }
 
-# Creating a resource reference point to be used by Terraform components 
-data "aws_caller_identity" "current" {}
-data "aws_partition" "current" {}
-data "aws_region" "current" {}
-data "aws_availability_zones" "available" {
-  state = "available"
-}
+# output "json_data" {
+#   value = length(local.json_data.vpc)
+# }
 
-locals {
-  json_data = jsondecode(file("variables.json"))
-}
+# module "vpc" {
+#   count  = length(local.json_data.vpc)
+#   source = "terraform-aws-modules/vpc/aws"
 
-output "json_data" {
-  value = length(local.json_data.vpc)
-}
 
+#   name = local.json_data.vpc[count.index].name
+#   cidr = local.json_data.vpc[count.index].cidr
+
+#   azs             = local.json_data.vpc[count.index].azs
+#   private_subnets = local.json_data.vpc[count.index].private_subnets
+#   public_subnets  = local.json_data.vpc[count.index].public_subnets
+
+#   enable_nat_gateway = local.json_data.vpc[count.index].enable_nat_gateway
+#   enable_vpn_gateway = local.json_data.vpc[count.index].enable_vpn_gateway
+
+#   tags = {
+#     Terraform   = "${local.json_data.vpc[count.index].tags.Terraform}"
+#     Environment = "${local.json_data.vpc[count.index].tags.Terraform}"
+#   }
+# }
 
 
 module "vpc" {
-  count  = length(local.json_data.vpc)
+  count  = length(var.v_vpc)
   source = "terraform-aws-modules/vpc/aws"
 
 
@@ -42,5 +48,4 @@ module "vpc" {
     Environment = "${local.json_data.vpc[count.index].tags.Terraform}"
   }
 }
-
 
---
+++ variable.tf
@@ -0,0 +1,13 @@
+variable "v_vpc" {
+  type = list(object({
+    name               = string,
+    cidr               = string,
+    azs                = list(string),
+    private_subnets    = list(string),
+    public_subnets     = list(string),
+    enable_nat_gateway = bool,
+    enable_vpn_gateway = bool,
+    tags               = map(string)
+    })
+  )
+}
