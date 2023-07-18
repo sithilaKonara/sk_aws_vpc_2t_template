6dc1c06f18151b136ad0c0f3c06a067a787edf24
mppuser-at-793820306412 <Sithila.Madushanka@pearson.com>
2023-07-18 08:07:17 +0530

    test for_each loop

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# module "vpc" {
+# module "vpcs" {
 #   count  = length(var.v_vpc)
 #   source = "terraform-aws-modules/vpc/aws"
 
@@ -19,8 +19,29 @@
 #   }
 # }
 
-module "vpc" {
-  for_each = { for vpc in var.v_vpc : vpc.name => vpc }
+# module "vpcs" {
+#   for_each = { for vpc in var.v_vpc : vpc.name => vpc }
+#   source   = "terraform-aws-modules/vpc/aws"
+
+
+#   name = each.value.name
+#   cidr = each.value.cidr
+
+#   azs             = each.value.azs
+#   private_subnets = each.value.private_subnets
+#   public_subnets  = each.value.public_subnets
+
+#   enable_nat_gateway = each.value.enable_nat_gateway
+#   enable_vpn_gateway = each.value.enable_vpn_gateway
+
+#   tags = {
+#     Terraform   = "${each.value.tags["Terraform"]}"
+#     Environment = "${each.value.tags["Environment"]}"
+#   }
+# }
+
+module "vpcs" {
+  for_each = toset(var.v_vpc)
   source   = "terraform-aws-modules/vpc/aws"
 
 
@@ -39,4 +60,3 @@ module "vpc" {
     Environment = "${each.value.tags["Environment"]}"
   }
 }
-
